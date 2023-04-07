806af65b6fff9097705190d02018f518fd1f41b4
Zade Viggers <zade@viggers.net>
2023-04-07 13:37:36 +1200

    Update database with new changes

--- internal-1-dictionary/setup_database.sql
+++ internal-1-dictionary/setup_database.sql
@@ -1,30 +1,42 @@
-CREATE TABLE "Users" (
-    "ID" INTEGER NOT NULL UNIQUE,
-    "Name" TEXT NOT NULL,
-    "Teacher" INTEGER,
-    PRIMARY KEY("ID" AUTOINCREMENT)
-) STRICT;
+CREATE TABLE
+	"Users" (
+		"ID" INTEGER NOT NULL UNIQUE,
+		"Name" TEXT NOT NULL,
+		"Teacher" INTEGER,
+		PRIMARY KEY ("ID" AUTOINCREMENT)
+	) STRICT;
 
-CREATE TABLE "Images" (
-    "ID" INTEGER NOT NULL UNIQUE,
-    "Description" TEXT NOT NULL,
-    "AssertSlug" TEXT NOT NULL,
-    PRIMARY KEY("ID" AUTOINCREMENT)
-) STRICT;
+CREATE TABLE
+	"Images" (
+		"ID" INTEGER NOT NULL UNIQUE,
+		"Description" TEXT NOT NULL,
+		"AssertSlug" TEXT NOT NULL,
+		PRIMARY KEY ("ID" AUTOINCREMENT)
+	) STRICT;
 
-CREATE TABLE "Words" (
-    "ID" INTEGER NOT NULL UNIQUE,
-    "MaoriSpelling" TEXT NOT NULL,
-    "EnglishSpelling" TEXT NOT NULL,
-    "EnglishDefinition" TEXT NOT NULL,
-    "YearLevelFirstEncountered" INTEGER NOT NULL,
-    "ImageID" INTEGER NOT NULL,
-    "CreatedBy" INTEGER NOT NULL,
-    "CreatedAt" INTEGER NOT NULL,
-    "LastModifiedBy" INTEGER NOT NULL,
-    "LastModifiedAt" INTEGER NOT NULL,
-    FOREIGN KEY("ImageID") REFERENCES "Images"("ID"),
-    FOREIGN KEY("CreatedBy") REFERENCES "Users"("ID"),
-    FOREIGN KEY("LastModifiedAt") REFERENCES "Users"("ID"),
-    PRIMARY KEY("ID" AUTOINCREMENT)
-) STRICT;
+CREATE TABLE
+	"Categories" (
+		"ID" INTEGER NOT NULL UNIQUE,
+		"EnglishName" INTEGER NOT NULL,
+		PRIMARY KEY ("ID" AUTOINCREMENT)
+	) STRICT;
+
+CREATE TABLE
+	"Words" (
+		"ID" INTEGER NOT NULL UNIQUE,
+		"MaoriSpelling" TEXT NOT NULL,
+		"EnglishSpelling" TEXT NOT NULL,
+		"EnglishDefinition" TEXT NOT NULL,
+		"YearLevelFirstEncountered" INTEGER NOT NULL,
+		"ImageID" INTEGER NOT NULL,
+		"CreatedBy" INTEGER NOT NULL,
+		"CreatedAt" INTEGER NOT NULL,
+		"LastModifiedBy" INTEGER NOT NULL,
+		"LastModifiedAt" INTEGER NOT NULL,
+		"CategoryID" INTEGER NOT NULL,
+		PRIMARY KEY ("ID" AUTOINCREMENT),
+		FOREIGN KEY ("CategoryID") REFERENCES "Categories" ("ID"),
+		FOREIGN KEY ("CreatedBy") REFERENCES "Users" ("ID"),
+		FOREIGN KEY ("ImageID") REFERENCES "Images" ("ID"),
+		FOREIGN KEY ("LastModifiedAt") REFERENCES "Users" ("ID")
+	) STRICT;
